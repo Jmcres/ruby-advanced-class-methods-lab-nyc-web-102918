c4daeab312c91ceae585d8464d2d4eb5439e94d9
Jason Crescenzo <crescenzojay@gmail.com>
2018-11-04 23:04:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,25 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+ 
+ def initialize (name,artist_name)
+    @name = name
+    @artist_name =artist_name
+    
+  end
+ 
+ def song.create 
+   c=self.new 
+   c.save
+ end
+ 
+ 
   def self.all
     @@all
   end
-
+ 
   def save
-    self.class.all << self
+   @@all << self
   end
-
+ 
 end
